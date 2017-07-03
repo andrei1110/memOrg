b8081d8cb6eebe006adcea9ce0319d2e44541f0c
andrei1110 <actoledo.andrei@gmail.com>
2017-07-03 15:55:56 -0300

    primeiro commit
    Inicialização do banco de dados e primeiros dados da estrutura

---
+++ struct.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+/*
+	Trabalho sobre memória
+	Organização de computadores
+	Andrei Toledo, Jardel Anton e Marcelo Acordi
+*/
+
+define("MAXMEM", 256); //TAMANHO MÁXIMO DA MEMÓRIA PRINCIPAL (blocos)
+define("MAXCACHE", 256); //TAMANHO MÁXIMO DA CACHE (quadros)
+define("MAXWORD", 4); //TAMANHO DA PALAVRA (bytes)
+
+/*
+	Para a simulação da cache, usaremos a memória principal do usuário (o que esta sendo exibido na tela)
+	Para a memória principal, usaremos um banco de dados para simular, esse banco de dados será limpo toda vez que o navegador for atualizado ou reiniciado.
+*/
+
+//DADOS DO BANCO DE DADOS
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DB", "mem");
+
+
+function connect(){ //FUNÇÃO PARA CONEXÃO NO BANCO DE DADOS
+	$conn = mysql_connect(HOST, USER, PASSWORD) or print(msql_error());
+	mysql_select_db(DB, $conn);
+	return 1;
+}
+
+function statrDB(){//INICIALIZAR O BANCO
+	connect();
+	$query = "CREATE OR REPLACE TABLE mem(
+				block INT(4), PRIMARY KEY)";
+	$sql = mysql_query($query) or print(mysql_error());
+}
+
+
+
+?>
