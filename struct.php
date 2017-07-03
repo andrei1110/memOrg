3b68a70a35b5d49e5a0c78cc76d287181d483c7b
andrei1110 <actoledo.andrei@gmail.com>
2017-07-03 16:13:10 -0300

    alterações na criação de tabelas

--- struct.php
+++ struct.php
@@ -25,14 +25,22 @@ define("DB", "mem");
 function connect(){ //FUNÇÃO PARA CONEXÃO NO BANCO DE DADOS
 	$conn = mysql_connect(HOST, USER, PASSWORD) or print(msql_error());
 	mysql_select_db(DB, $conn);
-	return 1;
+	return $conn;
 }
 
-function statrDB(){//INICIALIZAR O BANCO
+function startDB(){//INICIALIZAR O BANCO
 	connect();
 	$query = "CREATE OR REPLACE TABLE mem(
-				block INT(4), PRIMARY KEY)";
+				block INT(9), PRIMARY KEY)";
 	$sql = mysql_query($query) or print(mysql_error());
+	$query = "CREATE OR REPLACE TABLE block(
+				cell INT(4), PRIMARY KEY),
+				info INT(1),
+				block INT(9) NOT NULL, 
+				FOREIGN KEY (block) REFERENCES mem(block)";
+	$sql = mysql_query($query) or print(mysql_error());
+	
+	mysql_close(connect());
 }
 
 
