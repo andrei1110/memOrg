3bf4c65b4ca20010d3bab12c425f43fdd6f1f1ae
andrei1110 <actoledo.andrei@gmail.com>
2017-07-06 19:35:41 -0300

    inclusão do jquery

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include("start.php");
 	<title>Trabalho Organização</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	
+	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
 <body>
